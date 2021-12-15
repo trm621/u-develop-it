b1d2bc2774c33fb8cfeeab146f5083674bc3bb07
trm621 <t.maher621@gmail.com>
2021-12-15 14:51:35 -0500

    connected server to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,18 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // 
+        password: 'kPpV^$n[g~s`UpqX%|,a1],W6_D?CoJ!2fSoZ8@OYMDw`|tNCv',
+        database: 'election' 
+    },
+    console.log('Connected to the election database.')
+)
 
 app.get('/', (req, res) => {
     res.json({
